ccb813fe38297012dc80c3fde884ff129ba21b20
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-17 11:36:55 +0200

    Dashboard+css

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,10 @@
-import Lights from './Lights';
-import Climate from './Climate';
-import Blinds from './Blinds';
-import Energy from './Energy';
+import Lights from '../Lights';
+import Climate from '../Climate';
+import Blinds from '../Blinds';
+import Energy from '../Energy';
+import './Dashboard.css';
 
 const Dashboard = () => {
-
   return (
     <main className="dashboard">
       <Lights />
